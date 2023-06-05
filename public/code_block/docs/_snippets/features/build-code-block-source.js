accc9210ff29592a64fd4933190a4abc708efcd5
rezabuddawla <rezabuddawlatuhin@gmail.com>
2023-06-05 12:06:53 +0600

    building block

---
+++ public/code_block/docs/_snippets/features/build-code-block-source.js
@@ -0,0 +1,19 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* globals window */
+
+import { Code } from '@ckeditor/ckeditor5-basic-styles';
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
+import { CKBox } from '@ckeditor/ckeditor5-ckbox';
+import { PictureEditing, ImageResize, AutoImage } from '@ckeditor/ckeditor5-image';
+import { LinkImage } from '@ckeditor/ckeditor5-link';
+
+// Umberto combines all `packages/*/docs` into the `docs/` directory. The import path must be valid after merging all directories.
+import ClassicEditor from '../build-classic';
+
+ClassicEditor.builtinPlugins.push( CodeBlock, Code, PictureEditing, ImageResize, AutoImage, LinkImage, CKBox );
+
+window.ClassicEditor = ClassicEditor;
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\DictionaryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/login', [LoginController::class, 'getPage']);
+Route::get('/register', [RegisterController::class, 'getPage']);
+Route::get('/', [HomeController::class, 'getPage']);
+
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout']);
+Route::post('/upload', [HomeController::class, 'uploadiamge'])->name('ckeditor.upload');
+Route::post('/post', [HomeController::class, 'post']);
+
+Route::get('/dictionary', [DictionaryController::class, 'getPage']);
+Route::post('/user_dictionary', [DictionaryController::class, 'insert']);
+
+Route::get('/dictionary/view/{item}', [DictionaryController::class, 'view_item']);
